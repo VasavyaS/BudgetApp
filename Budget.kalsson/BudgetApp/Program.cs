dfd083453bf4b26f0617d0936e0d186715aaf881
jklogi <jk@logimark.se>
2025-02-10 12:39:10 +0100

    Add SQL Server database connection and configure DbContext
    Integrated a connection string for SQL Server in appsettings.json. Registered ApplicationDbContext in the dependency injection container to enable database interactions using Entity Framework Core. These changes set up the foundational database configuration for the application.

--- Budget.kalsson/BudgetApp/Program.cs
+++ Budget.kalsson/BudgetApp/Program.cs
@@ -1,7 +1,15 @@
+using BudgetApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register ApplicationDbContext with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
