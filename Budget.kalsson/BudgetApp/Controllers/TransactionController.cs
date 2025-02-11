8093690c6be1d90106406e85431969a796373cd7
jklogi <jk@logimark.se>
2025-02-11 09:35:57 +0100

    Refactor transaction views and controller for modal support
    Replaced standard views with partial views for Create, Edit, and Delete actions to enable modal-based interactions. Updated the TransactionController to handle AJAX requests and adapted the Index view to dynamically load modals for better user experience. Simplified some controller methods and cleaned up redundant code.

--- Budget.kalsson/BudgetApp/Controllers/TransactionController.cs
+++ Budget.kalsson/BudgetApp/Controllers/TransactionController.cs
@@ -1,175 +1,103 @@
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BudgetApp.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using BudgetApp.Data;
-using BudgetApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace BudgetApp.Controllers
+public class TransactionController : Controller
 {
-    public class TransactionController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public TransactionController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public TransactionController(ApplicationDbContext context)
+    public async Task<IActionResult> Index()
+    {
+        var transactions = _context.Transactions.Include(t => t.Category);
+        return View(await transactions.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+        return PartialView("_CreatePartial");
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Amount,Date,CategoryId")] Transaction transaction)
+    {
+        if (ModelState.IsValid)
         {
-            _context = context;
-        }
-
-        // GET: Transaction
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.Transactions.Include(t => t.Category);
-            return View(await applicationDbContext.ToListAsync());
-        }
-
-        // GET: Transaction/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var transaction = await _context.Transactions
-                .Include(t => t.Category)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (transaction == null)
-            {
-                return NotFound();
-            }
-
-            return View(transaction);
-        }
-
-        // GET: Transaction/Create
-        public IActionResult Create()
-        {
-            ViewBag.CategoryId = new SelectList(_context.Categories, "Id", "Name");
-            return View();
-        }
-
-        // POST: Transaction/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Transaction transaction)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(transaction);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            // Log all ModelState errors for debugging
-            foreach (var state in ModelState)
-            {
-                foreach (var error in state.Value.Errors)
-                {
-                    Console.WriteLine($"Field: {state.Key}, Error: {error.ErrorMessage}");
-                }
-            }
-
-            // Repopulate the dropdown list in case of invalid ModelState
-            ViewBag.CategoryId = new SelectList(_context.Categories, "Id", "Name", transaction.CategoryId);
-            return View(transaction);
-        }
-
-        // GET: Transaction/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var transaction = await _context.Transactions.FindAsync(id);
-            if (transaction == null)
-            {
-                return NotFound();
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", transaction.CategoryId);
-            return View(transaction);
-        }
-
-        // POST: Transaction/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Amount,Date,CategoryId")] Transaction transaction)
-        {
-            if (id != transaction.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(transaction);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TransactionExists(transaction.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", transaction.CategoryId);
-            return View(transaction);
-        }
-
-        // GET: Transaction/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var transaction = await _context.Transactions
-                .Include(t => t.Category)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (transaction == null)
-            {
-                return NotFound();
-            }
-
-            return View(transaction);
-        }
-
-        // POST: Transaction/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var transaction = await _context.Transactions.FindAsync(id);
-            if (transaction != null)
-            {
-                _context.Transactions.Remove(transaction);
-            }
-
+            _context.Add(transaction);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Json(new { success = true });
         }
 
-        private bool TransactionExists(int id)
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", transaction.CategoryId);
+        return PartialView("_CreatePartial", transaction);
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var transaction = await _context.Transactions.FindAsync(id);
+        if (transaction == null)
+            return NotFound();
+
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", transaction.CategoryId);
+        return PartialView("_EditPartial", transaction);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Amount,Date,CategoryId")] Transaction transaction)
+    {
+        if (id != transaction.Id)
+            return NotFound();
+
+        if (ModelState.IsValid)
         {
-            return _context.Transactions.Any(e => e.Id == id);
+            _context.Update(transaction);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true });
         }
+
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", transaction.CategoryId);
+        return PartialView("_EditPartial", transaction);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var transaction = await _context.Transactions
+                            .Include(t => t.Category)
+                            .FirstOrDefaultAsync(m => m.Id == id);
+        if (transaction == null)
+            return NotFound();
+
+        return PartialView("_DeletePartial", transaction);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var transaction = await _context.Transactions.FindAsync(id);
+        if (transaction != null)
+        {
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+        }
+
+        return Json(new { success = true });
     }
 }
