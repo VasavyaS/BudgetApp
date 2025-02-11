f83376c6de69eb67e62e4afdee2254f840e367e8
jklogi <jk@logimark.se>
2025-02-11 09:25:21 +0100

    Enable modal-based CRUD operations for categories
    Replaced traditional page-based CRUD actions with modal-based partial views for a smoother user experience. Added AJAX functionality to handle form submissions and page updates dynamically. Updated layout and scripts to include support for modal interactions and validation.

--- Budget.kalsson/BudgetApp/Controllers/CategoryController.cs
+++ Budget.kalsson/BudgetApp/Controllers/CategoryController.cs
@@ -1,157 +1,92 @@
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BudgetApp.Models;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using BudgetApp.Data;
-using BudgetApp.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace BudgetApp.Controllers
+public class CategoryController : Controller
 {
-    public class CategoryController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public CategoryController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public CategoryController(ApplicationDbContext context)
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Categories.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return PartialView("_CreatePartial");
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
+    {
+        if (ModelState.IsValid)
         {
-            _context = context;
-        }
-
-        // GET: Category
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Categories.ToListAsync());
-        }
-
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
-        // GET: Category/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Category/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(category);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
-        // GET: Category/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Category/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
-        {
-            if (id != category.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(category);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExists(category.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
-        // POST: Category/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var category = await _context.Categories.FindAsync(id);
-            if (category != null)
-            {
-                _context.Categories.Remove(category);
-            }
-
+            _context.Add(category);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Json(new { success = true });
         }
+        return PartialView("_CreatePartial", category);
+    }
 
-        private bool CategoryExists(int id)
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+            return NotFound();
+
+        return PartialView("_EditPartial", category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
+    {
+        if (id != category.Id)
+            return NotFound();
+
+        if (ModelState.IsValid)
         {
-            return _context.Categories.Any(e => e.Id == id);
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true });
         }
+        return PartialView("_EditPartial", category);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+            return NotFound();
+
+        return PartialView("_DeletePartial", category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category != null)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+        return Json(new { success = true });
     }
 }
---
+++ Budget.kalsson/BudgetApp/wwwroot/js/modal-ajax.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    // Open the modal when a button is clicked
+    $(document).on('click', '.open-modal', function (e) {
+        e.preventDefault();
+
+        var url = $(this).data("url");
+
+        $.get(url, function (response) {
+            $("#modalContent").html(response);
+            $("#modalContainer").modal("show");
+        });
+    });
+
+    // Handle form submission
+    $(document).on('submit', 'form', function (e) {
+        e.preventDefault();
+
+        var form = $(this);
+        var url = form.attr("action");
+        var method = form.attr("method");
+        var data = form.serialize();
+
+        $.ajax({
+            url: url,
+            method: method,
+            data: data,
+            success: function (response) {
+                if (response.success) {
+                    $("#modalContainer").modal("hide");
+                    location.reload(); // Refresh page or table contents
+                } else {
+                    $("#modalContent").html(response); // Reload modal with validation errors
+                }
+            }
+        });
+    });
+});
